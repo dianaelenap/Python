37bd7ae7f2e7c666da1e73677b9c1d1dc0e0ecc6
dianaelenap <diana_elena98@yahoo.com>
2020-12-14 17:06:24 +0200

    main

---
+++ main.py
@@ -0,0 +1,54 @@
+from produs import Product
+import sqlite3
+
+conn = sqlite3.connect('Python/produse.db')
+
+c = conn.cursor()
+
+querry1 = "SELECT * FROM stoc"
+querry2 = "SELECT * FROM vanzari"
+
+stoc = c.execute(querry1)
+
+lista_stoc = []
+
+for row in stoc:
+    p = Product(row[1],row[2])
+    lista_stoc.append(p)
+
+vanzari = c.execute(querry2)
+
+lista_vanzari = []
+cantitate_zi = []
+
+for row in vanzari:
+    p = Product(row[1],row[2])
+    lista_vanzari.append(p)
+    cantitate_zi.append(row[2]/row[3])
+
+conn.commit()
+
+conn.close()
+
+if __name__ == "__main__":
+    time_period = int(input("Perioada de timp(zile): "))
+
+    cantitate_necesara = []
+
+    for i in range(0,len(cantitate_zi)):
+        cantitate_necesara.append(cantitate_zi[i]*time_period)
+
+    lista_cumparaturi = []
+
+    for i in range(0,len(lista_stoc)):
+        diferenta = lista_stoc[i].quantity - cantitate_necesara[i]
+        diferenta = round(diferenta)
+        if(diferenta >= 0):
+            continue
+        else:
+            diferenta = diferenta * (-1)
+            p = Product(lista_stoc[i].name,diferenta)
+            lista_cumparaturi.append(p)
+
+    for x in lista_cumparaturi:
+        print("Pentru %s trebuie cumparat %d " % (x.name,x.quantity))
--- produs.py
+++ produs.py
@@ -7,9 +7,3 @@ class Product:
     def __str__(self):
         return "Produsul %s este in cantitate de %d" % (self.name,self.quantity)
 
-    def diff(self,produs):
-        if(self.quantity > produs.quantity):
-            diff = self.quantity - produs.quantity
-        elif(self.quantity <= produs.quantity):
-            diff = produs.quantity - self.quantity
-        return diff
